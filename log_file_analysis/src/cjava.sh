71f40388ddb77c10f35f15fc443bf443e25797a3
gregorthebigmac <gregorthebigmac@gmail.com>
2019-09-28 02:58:51 -0500

    fixed compilation script to quit if encounters an error

--- log_file_analysis/src/cjava.sh
+++ log_file_analysis/src/cjava.sh
@@ -3,6 +3,6 @@
 input_java_src_dir="$1"
 jar_file="$2".jar
 
-javac -classpath `hadoop classpath` "$input_java_src_dir"/*.java
-jar cvf "$jar_file" "$input_java_src_dir"/*.class
-chmod 777 "$jar_file"
+javac -classpath `hadoop classpath` "$input_java_src_dir"/*.java \
+&& jar cvf "$jar_file" "$input_java_src_dir"/*.class \
+&& chmod 777 "$jar_file"
