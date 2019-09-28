1cc1bdff54faeb8cb4fd98d41742666d312b150a
gregorthebigmac <gregorthebigmac@gmail.com>
2019-09-27 23:21:06 -0500

    fixed typos.

--- averagewordlength/src/stubs/LetterMapper.java
+++ averagewordlength/src/stubs/LetterMapper.java
@@ -10,10 +10,10 @@ public class LetterMapper extends Mapper<LongWritable, Text, Text, IntWritable> 
 	@Override
 	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 		String file_line = value.toString();
-		for (String word : line.split("\\W+")) {
+		for (String word : file_line.split("\\W+")) {
 			if (word.length() > 0) {
 				String first_letter = word.substring(0, 1);
-				context.write(new Text(letter), new IntWritable(word.length()));
+				context.write(new Text(first_letter), new IntWritable(word.length()));
 			}
 		}
 	}
