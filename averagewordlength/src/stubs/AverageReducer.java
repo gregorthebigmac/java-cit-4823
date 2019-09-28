10d8c801d46aba89a195bfc542d0551057c40349
gregorthebigmac <gregorthebigmac@gmail.com>
2019-09-27 23:12:42 -0500

    finished, but not tested.

--- averagewordlength/src/stubs/AverageReducer.java
+++ averagewordlength/src/stubs/AverageReducer.java
@@ -7,15 +7,15 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class AverageReducer extends Reducer<Text, IntWritable, Text, DoubleWritable> {
-
-  @Override
-  public void reduce(Text key, Iterable<IntWritable> values, Context context)
-  	throws IOException, InterruptedException {
-        String line = value.toString();
-		for (String word : line.split("\\W+")) {
-			if (word.length() > 0) {
-				context.write(new Text(word), new IntWritable);
-			}
+	@Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+		long sum = 0;
+		long count = 0;
+		for (IntWritable values: values) {
+			sum = sum + values.get();
+			count++;
 		}
-  }
+		if (count > 0)
+			context.write(key, new DoubleWritable((double)sum / (double)count));
+	}
 }
--- averagewordlength/src/stubs/LetterMapper.java
+++ averagewordlength/src/stubs/LetterMapper.java
@@ -7,14 +7,14 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class LetterMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-
-  @Override
-  public void map(LongWritable key, Text value, Context context)
-      throws IOException, InterruptedException {
-
-    /*
-     * TODO implement
-     */
-
-  }
+	@Override
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		String file_line = value.toString();
+		for (String word : line.split("\\W+")) {
+			if (word.length() > 0) {
+				String first_letter = word.substring(0, 1);
+				context.write(new Text(letter), new IntWritable(word.length()));
+			}
+		}
+	}
 }
