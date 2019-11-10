c2097c7d3a60f3219f81eeaa2184cdfc655b874d
gregorthebigmac <gregorthebigmac@gmail.com>
2019-11-10 14:43:20 -0600

    forgot to include --update option. Now it won't overwrite if target is newer.

--- sync_files.sh
+++ sync_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Pushing files to big-data VM:"
-rsync -var -P -e ssh ~/git_repos/java-cit-4823/ training@big-data:/home/training/training_materials/developer/exercises/ \
+rsync -var --update -P -e ssh ~/git_repos/java-cit-4823/ training@big-data:/home/training/training_materials/developer/exercises/ \
 && echo "
 Pulling files from big-data VM:
 " \
-&& rsync -var -P -e ssh training@big-data:/home/training/training_materials/developer/exercises/ ~/git_repos/java-cit-4823/
+&& rsync -var --update -P -e ssh training@big-data:/home/training/training_materials/developer/exercises/ ~/git_repos/java-cit-4823/
