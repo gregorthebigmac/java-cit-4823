c98a1ffd9e81dbc6fdc46a29f61b3ea747dde503
gregorthebigmac <gregorthebigmac@gmail.com>
2019-11-10 14:42:24 -0600

    updated sync_files to go both ways

--- sync_files.sh
+++ sync_files.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-rsync -vr -P -e ssh ~/git_repos/java-cit-4823/ training@big-data:/home/training/training_materials/developer/exercises/
+echo "Pushing files to big-data VM:"
+rsync -var -P -e ssh ~/git_repos/java-cit-4823/ training@big-data:/home/training/training_materials/developer/exercises/ \
+&& echo "
+Pulling files from big-data VM:
+" \
+&& rsync -var -P -e ssh training@big-data:/home/training/training_materials/developer/exercises/ ~/git_repos/java-cit-4823/
