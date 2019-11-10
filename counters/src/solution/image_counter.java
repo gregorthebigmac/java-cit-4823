a5fee407a7a577ef40db0b01d5fdad1131f68af3
gregorthebigmac <gregorthebigmac@gmail.com>
2019-11-10 10:29:07 -0600

    added 5-3 hw. Not tested yet.

---
+++ counters/src/solution/image_counter.java
@@ -0,0 +1,49 @@
+package solution;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class image_counter extends Configured implements Tool {
+	@override public int run(String[] args) throws Exception {
+		if (args.length != 2) {
+			System.out.println("Usage: image_counter [input/dir] [output/dir]");
+			return -1;
+		}
+		Job job = Job.getInstance();
+		job.setJarByClass(image_counter.class);
+		job.setJobName("image counter");
+		
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		Job.setMapperClass(image_counter_mapper.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		job.setNumReduceTasks(0);
+		
+		boolean success = job.waitForCompletion(true);
+		if (success) {
+			long jpg = job.getCounters().findCounter("image counter", "jpg").getValue();
+			long gif = job.getCounters().findCounter("image counter", "gif").getValue();
+			long other = job.getCounters().findCounter("image counter", "other").getValue();
+			System.out.println("jpg = " + jpg);
+			System.out.println("gif = " + gif);
+			System.out.println("other = " + other);
+			return 0;
+		}
+		else return 1;
+	}
+	public static void main(String[] args) throws Exception {
+		int exit_code = ToolRunner.run(new Configuration(), new image_counter(), args);
+		System.exit(exit_code);
+	}
+}
---
+++ counters/src/solution/image_counter_mapper.java
@@ -0,0 +1,26 @@
+package solution;
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class image_counter_mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	@Override public void map(LongWritable key, Text value, Context context) throws Exception, InterruptedException {
+		String[] fields = value.toString().split("\"");
+		if (fields.length > 1) {
+			String request = fields[1];
+			fields = request.split(" ");
+			if (fields.length > 1) {
+				String filename = fields[1].toLowerCase();
+				if (filename.endsWith(".jpg"))
+					context.getCounter("image_counter", "jpg").increment(1);
+				else if (filename.endsWith(".gif"))
+					context.getCounter("image_counter", "gif").increment(1);
+				else
+					context.getCounter("image_counter", "other").increment(1);
+			}
+		}
+	}
+}
